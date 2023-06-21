5d3e5e46907bb9150ef1c65cfdb3b33f391e91b5
mrdotb <mrdotb@protonmail.com>
2023-06-21 12:44:20 +0200

    try to build crates

--- lib/resvg/native.ex
+++ lib/resvg/native.ex
@@ -1,29 +1,29 @@
 defmodule Resvg.Native do
   @moduledoc false
-  mix_config = Mix.Project.config()
-  version = mix_config[:version]
-  github_url = mix_config[:package][:links]["GitHub"]
+  # mix_config = Mix.Project.config()
+  # version = mix_config[:version]
+  # github_url = mix_config[:package][:links]["GitHub"]
 
-  targets = ~w(
-    arm-unknown-linux-gnueabihf
-    aarch64-apple-darwin
-    aarch64-unknown-linux-gnu
-    aarch64-unknown-linux-musl
-    riscv64gc-unknown-linux-gnu
-    x86_64-apple-darwin
-    x86_64-pc-windows-gnu
-    x86_64-pc-windows-msvc
-    x86_64-unknown-linux-gnu
-    x86_64-unknown-linux-musl
-  )s
+  # targets = ~w(
+  #   arm-unknown-linux-gnueabihf
+  #   aarch64-apple-darwin
+  #   aarch64-unknown-linux-gnu
+  #   aarch64-unknown-linux-musl
+  #   riscv64gc-unknown-linux-gnu
+  #   x86_64-apple-darwin
+  #   x86_64-pc-windows-gnu
+  #   x86_64-pc-windows-msvc
+  #   x86_64-unknown-linux-gnu
+  #   x86_64-unknown-linux-musl
+  # )s
 
- use RustlerPrecompiled,
-    otp_app: :resvg,
-    crate: "resvg",
-    base_url: "#{github_url}/releases/download/v#{version}",
-    force_build: System.get_env("RESVG_BUILD") in ["1", "true"],
-    version: version,
-    targets: targets
+ # use RustlerPrecompiled,
+ #    otp_app: :resvg,
+ #    crate: "resvg",
+ #    base_url: "#{github_url}/releases/download/v#{version}",
+ #    force_build: System.get_env("RESVG_BUILD") in ["1", "true"],
+ #    version: version,
+ #    targets: targets
 
   use Rustler, otp_app: :resvg, crate: :resvg
 
